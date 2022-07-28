d18dc0c6a0afa1876ef412f52548fd111a2dbce2
hellocarloshenrique <carlos.damasceno182@gmail.com>
2022-07-28 07:18:57 -0300

    adicionando array e for

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ let niveis = ["Muito abaixo do peso",
               "Obesidade grau II",
               "Obesidade grau III"]
 
+let pessoas = ["Carlos Henrique", "Elizabeth", "Elias"];
+let massas = [80, 70, 60];
+let alturas = [1.70, 1.90, 1.80];            
+
+// funcoes --------------------------------------
+
 function calcularIMC (massa, altura) {
   let IMC = massa / (altura ** 2);
 
   return IMC;
 }
 
-// funcoes --------------------------------------
 function verificaNivel (IMC) {
   let nivel = 0;
 
@@ -39,5 +44,21 @@ function verificaNivel (IMC) {
 
 // execucao --------------------------------------
 
-let nivel = verificaNivel(calcularIMC(150,1.90))
-console.log(niveis[nivel])
+/*let indice = 0*/
+
+for(let indice = 0; indice < pessoas.length; indice++) {
+  let pessoa = pessoas[indice]
+  let massa = massas[indice]
+  let altura = alturas[indice]
+  
+  let IMC = calcularIMC(massa,altura)
+  let nivel = verificaNivel(IMC)
+  
+  console.log("Nome: "+ pessoa)
+  console.log("Peso: "+ massa)
+  console.log("Altura: "+ altura)
+  console.log("IMC: "+ IMC.toFixed(2))
+  
+  console.log("Nível: " + niveis[nivel])
+  console.log(" ")
+}
